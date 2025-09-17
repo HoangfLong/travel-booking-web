fedf8cc962c1a9e7a76bff55290a6a4495abc804
hoangflong <hoanglg3009@gmail.com>
2025-09-17 15:11:59 +0700

    api-admin

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\TourController as AdminTourController;
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
@@ -12,17 +13,32 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
-    
+
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
-    //Tours
-    Route::get('/tours/trashed', [TourController::class, 'getAllTrashed']);
-    Route::post('tours/create', [TourController::class, 'store']);
-    Route::post('tours/update/{id}', [TourController::class, 'update']);
+    // Tours public for user
     Route::get('/tours', [TourController::class, 'index']);
     Route::get('/tours/{id}', [TourController::class, 'show']);
-    Route::delete('/tours/{id}', [TourController::class, 'destroy']);
+
+    //Tours
+    Route::middleware(['auth:sanctum'])->group(function () {
+
+        // Admin tours
+        Route::middleware('role:admin')->group(function () {
+
+            Route::get('/admin/dashboard', [AdminTourController::class, 'index']);
+
+            Route::post('tours/create', [AdminTourController::class, 'store']);
+            Route::post('tours/update/{id}', [AdminTourController::class, 'update']);
+            Route::delete('/tours/{id}', [AdminTourController::class, 'destroy']);
+            Route::get('/tours/trashed', [AdminTourController::class, 'getAllTrashed']);
+            Route::get('/tours/restore/{id}', [AdminTourController::class, 'restore']);
+        });
+
+        // Logout
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
